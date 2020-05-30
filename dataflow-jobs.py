8ec3bba0d7c503ea966bd4572d6939ad39d61993
Alexis Tejeda <alexis.tejeda@gmail.com>
2020-05-30 23:23:57 +0000

    Working retreival of dataflow indexes

--- dataflow-jobs.py
+++ dataflow-jobs.py
@@ -11,6 +11,9 @@
 # https://cloud.google.com/dataflow/docs/reference/rest
 # https://developers.google.com/resources/api-libraries/documentation/dataflow/v1b3/python/latest/index.html
 
+import sys
+if sys.version_info[0] < 3: raise Exception('python >= 3.x supported')
+
 import os
 import json
 import socket
@@ -30,7 +33,9 @@ from google.api_core.exceptions import GoogleAPIError
 from googleapiclient.errors import HttpError
 from googleapiclient.http import BatchHttpRequest
 
-logging.basicConfig(format='[%(asctime)s %(levelname)s] - %(message)s', level=logging.INFO)
+logging.basicConfig(format='[%(asctime)s %(levelname)s] - %(message)s',
+                    level=logging.INFO)
+
 
 def dataflow_debug(service_jobs, projectId, location, jobId):
     return service_jobs.debug().getConfig(
@@ -55,15 +60,15 @@ def dataflow_messages(service_jobs, projectId, location, jobId):
     
         messages.append(response)
 
-        
         pageToken = response.get('nextPageToken')
-        if pageToken:
+        if not pageToken:
             break
 
     return messages
 
 
 def dataflow_snapshots(service_jobs, projectId, location, jobId):
+    # TODO: snapshots are not available in region us-east1" 
     return service_jobs.snapshots().list(
             projectId=projectId,
             location=location,
@@ -72,6 +77,7 @@ def dataflow_snapshots(service_jobs, projectId, location, jobId):
 
 
 def dataflow_workItems(service_jobs, projectId, location, jobId):
+    # TODO: returned "Bad Gateway
     return service_jobs.workItems().reportStatus(
             projectId=projectId,
             location=location,
@@ -96,7 +102,9 @@ def dataflow_details(service_jobs, projectId, location, jobId):
     ).execute()
 
 
-def dataflow_job(projectId, location, jobId, page):
+def dataflow_job(projectId, location, job, page):
+    jobId = job.get('id')
+    
     logging.info('page = %s, jobId = %s', page, jobId)
 
     service = build('dataflow', 'v1b3', cache_discovery=False)
@@ -107,18 +115,123 @@ def dataflow_job(projectId, location, jobId, page):
     functions = {
         'debug'     : dataflow_debug,
         'messages'  : dataflow_messages,
-        'snapshots' : dataflow_snapshots,
-        'workItem'  : dataflow_workItems,
+        #'snapshots' : dataflow_snapshots,
+        #'workItems' : dataflow_workItems,
         'metrics'   : dataflow_metrics,
         'details'   : dataflow_details,
     }
+    
+    responses = { k : v(*args) for k,v in functions.items() }
+    responses['page'] = page
+    responses['jobId'] = jobId
 
-    return { k : v(*args) for k,v in functions.items() }
+    return responses
+    
+def dataflow_jobs_read(projectId, location, file, output, workers):
+    logging.info('worker pool set to %d ', workers)
+    
+    pages = json.load(file)
+    logging.debug('pages loaded %s', str(file))
+
+    files = list()
+    failed = list()
+    
+    for page in pages:
+        pageId = page.get('page')
+        jobs = page.get('jobs')
+        
+        logging.debug('processing page = %d', pageId)
+
+        try:
+            with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
+                logging.debug(executor)
+            
+                futures = { executor.submit(
+                    dataflow_job, projectId, location, job, pageId
+                ) : job for job in jobs }
+                
+                responses = [future.result() for future in concurrent.futures.as_completed(futures)]
+
+                filepath = '{}{}{}_dataflow.{}.json'.format(
+                    output, os.path.sep, projectId, f'{pageId}'.zfill(3)
+                )
+            
+                logging.info('writing results into %s', filepath)
+                json.dump(responses, open(filepath, 'w'))
+
+                files.append(filepath)
+        except:
+            logging.exception('fail to process page %d', pageId)
+            failed.append(page)
+
+    if failed:
+        filepath = '{}{}{}_dataflow.failed.json'.format(
+            output, os.path.sep, projectId
+        )
+        logging.info('writing failed pages into %s', filepath)
+        json.dump(failed, open(filepath, 'w'))
+        
+    if len(files) > 1:
+    
+        logging.info('trying to write amalgamate all the results files in one')
+        try:
+            filepath = '{}{}{}_dataflow.full.json'.format(
+                output, os.path.sep, projectId
+            )
+            # inneficient as hell, but works !
+            json.dump(
+                [json.load(open(f)) for f in files], open(filepath, 'w')
+            )
+            logging.info('file written in %s', filepath)
+        except:
+            logging.exception('was\'t possible but results are kept')
+
+
+def dataflow_jobs_write(projectId, location, file, limit=0, datefrom=None, dateto=None):
+    pages, pageToken, page_counter = list(), None, 0
+
+    service = build('dataflow', 'v1b3', cache_discovery=False)
+    service_jobs = service.projects().locations().jobs()
+
+    pages = []
+
+    while not (limit and page_counter >= limit):
+        logging.info('-'*80)
+        logging.info('fetching jobs for page (%s) token = \'%s\'',
+                     page_counter, pageToken)
+
+        response = service_jobs.list(
+            projectId=projectId,
+            location=location,
+            view='JOB_VIEW_ALL',
+            pageToken=pageToken
+        ).execute()
+      
+        jobs = response.get('jobs')
+        logging.info('received total jobs = %d', len(jobs))
+
+        early_stop, jobs = dataflow_jobs_filter(jobs, datefrom, dateto)
+        logging.info('received total filtered = %d', len(jobs))
+        logging.info('early stop ? %s', early_stop)
+        
+        response['jobs'] = jobs
+        response['page'] = page_counter
+        pages.append(response)
+
+        page_counter += 1
+        
+        pageToken = response.get('nextPageToken')
+        if not pageToken or early_stop:
+            break
+
+    logging.info('*'*80)
+    logging.info('writing %d result pages', page_counter)
+    json.dump(pages, file, indent=4)
 
 
 def dataflow_jobs_filter(jobs, datefrom, dateto):
     if datefrom or dateto:
-        early_stop, filtered_jobs= False, list()
+        early_stop, filtered_jobs = False, list()
 
         for job in jobs: 
             createTime = dateutil.parser.parse(job.get('createTime'))
@@ -144,102 +257,14 @@ def dataflow_jobs_filter(jobs, datefrom, dateto):
         return False, jobs
 
 
-def dataflow_jobs_write(projectId, location, file, limit=0, datefrom=None, dateto=None):
-    pages, pageToken, page_counter = list(), None, 0
-
-    service = build('dataflow', 'v1b3', cache_discovery=False)
-    service_jobs = service.projects().locations().jobs()
-
-    pages = []
-
-    while not (limit and page_counter >= limit):
-        page_counter += 1
-
-        logging.info('-'*80)
-        logging.info('fetching jobs for page (%s) token = \'%s\'',
-                     page_counter, pageToken)
-
-        response = service_jobs.list(
-            projectId=projectId,
-            location=location,
-            view='JOB_VIEW_ALL',
-            pageToken=pageToken
-        ).execute()
-      
-        jobs = response.get('jobs')
-        logging.info('received total jobs = %d', len(jobs))
-
-        early_stop, jobs = dataflow_jobs_filter(jobs, datefrom, dateto)
-        logging.info('received total filtered = %d', len(jobs))
-        logging.info('early stop ? %s', early_stop)
-        
-        
-        response['jobs'] = jobs
-        pages.append(response)
-
-        pageToken = response.get('nextPageToken')
-        if pageToken or early_stop:
-            break
-   
-    logging.info('*'*80)
-    logging.info('writing %d result pages', page_counter)
-    json.dump(pages, file, indent=4)
-
-
-def dataflow_jobs_read(projectId, location):
-    jobs, pageToken, page_counter = list(), None, 0
-
-    while True:
-        logging.info('-'*80)
-
-        page_counter += 1
-        page_current = '{}'.format(page_counter).zfill(3)
-
-        logging.info('getting jobs from page (%s) token = \'%s\'', page_current, pageToken)
-
-        service = build('dataflow', 'v1b3', cache_discovery=False)
-        service_jobs = service.projects().locations().jobs()
-        
-        response = service_jobs.list(projectId=projectId,
-                                     location=location,
-                                     view='JOB_VIEW_ALL',
-                                     pageToken=pageToken).execute()
-      
-        jobs = response.get('jobs')
-        logging.info('received total jobs = %d', len(jobs))
-
-        with concurrent.futures.ProcessPoolExecutor(max_workers=100) as executor:
-            futures = {
-                    executor.submit(
-                        dataflow_job,
-                            projectId,
-                            location,
-                            job.get('id'),
-                            page_current
-                    ) : job.get('id') for job in jobs
-            }
-
-            responses = [future.result() for future in concurrent.futures.as_completed(futures)]
-
-            filename = 'us-east-1-dataflow-atejeda/dataflow/{}_dataflow_{}.json'.format(
-                    projectId,
-                    page_current
-            )
-            logging.info('writing results into %s', filename)
-            json.dump(jobs, open(filename, 'w'), indent=4)
-
-        #pageToken = response.get('nextPageToken') if 'nextPageToken' in response else None
-        if not pageToken: break
-
-    return jobs
-
-
 def string_date(arg0):
     return dateutil.parser.parse(arg0).replace(tzinfo=tzutc())
+
 
 def valid_directory(arg0):
     if os.path.isdir(arg0): return arg0
     raise argparse.ArgumentTypeError('directory doesn\'t exists')
+
 
 def index_mode(args):
     projectId = args.project
@@ -249,15 +274,18 @@ def index_mode(args):
     datefrom = args.datefrom
     dateto = args.dateto
 
-    try:
-        dataflow_jobs_write(projectId, location, file, limit, datefrom, dateto)
-    except KeyboardInterrupt:
-        logging.info('interrupted, nothing has been written')
-    except Exception as e:
-        logging.exception('there is an error')
-        
+    dataflow_jobs_write(projectId, location, file, limit, datefrom, dateto)
+
+    
 def full_mode(args):
-    pass
+    projectId = args.project
+    location = args.location
+    file = args.file
+    output = args.output
+    workers = args.workers
+
+    dataflow_jobs_read(projectId, location, file, output, workers)
+
 
 if __name__ == '__main__':
     socket.setdefaulttimeout(600)
@@ -273,18 +301,13 @@ if __name__ == '__main__':
     parser.add_argument('--location',
                         help='location (region)',
                         required=True)
-
+    
     # index mode arguments
 
     subparsers = parser.add_subparsers(help='sub-command help')
 
     index_group = subparsers.add_parser('index', help='index mode help')
     index_group.set_defaults(func=index_mode)
-    
-    index_group.add_argument('--file',
-                        help='the file to write write',
-                        type=argparse.FileType('w', encoding='UTF-8'),
-                        required=True)
     
     index_group.add_argument('--limit',
                         help='limit the result to N pages',
@@ -301,6 +324,11 @@ if __name__ == '__main__':
                         type=string_date,
                         default=None)
 
+    index_group.add_argument('--file',
+                        help='file to write the jobs to retrieve info later',
+                        type=argparse.FileType('w', encoding='UTF-8'),
+                        required=True)
+    
     # full mode arguments
 
     full_group = subparsers.add_parser('full', help='full mode help')
@@ -311,12 +339,24 @@ if __name__ == '__main__':
                         type=valid_directory,
                         required=True)
 
-    full_group.add_argument('--jobs',
-                        help='number of concurrent jobs',
+    full_group.add_argument('--workers',
+                        help='size of the worker pool',
                         type=int,
-                        default=multiprocessing.cpu_count())
+                        default=multiprocessing.cpu_count() - 1)
 
+    full_group.add_argument('--file',
+                        help='file to read which jobs to retrive the info',
+                        type=argparse.FileType('r', encoding='UTF-8'),
+                        required=True)
+    
+    
     # options
     
     args = parser.parse_args()
-    args.func(args)
+
+    try:
+        args.func(args)
+    except KeyboardInterrupt:
+        logging.info('interrupted, nothing has been written')
+    except Exception as e:
+        logging.exception('there is an error')
